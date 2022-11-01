ee162a2943bfbf1b00fe85690e7e04571dd19f5e
gvsrinivas82 <58799923+gvsrinivas82@users.noreply.github.com>
2022-10-31 23:47:24 -0400

    Add files via upload

---
+++ DistinctWords/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistinctWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string lsParagraph = "";
+
+            WordsOperations objWordsOperation = new WordsOperations();
+
+            /* Test Case 1 --> Pass empy string. It should display message "Input paragraph is empty." ==> SUCCESS */
+            objWordsOperation.DisplayDistinctWords(lsParagraph);
+
+            /* Test Case 2 */
+            /* Expected Output --> The --> 2, Universe --> 2 ... All other words --> 1 ==> SUCCESS */
+            lsParagraph = "The Universe is the most beautiful universe.";
+            objWordsOperation.DisplayDistinctWords(lsParagraph);
+
+            /* Test Case 3 */
+            /* With some Extra spaces */
+            /* Expected Output --> The --> 2, Universe --> 2 ... All other words --> 1 ==> SUCCESS */
+            lsParagraph = "The   Universe  is the most    beautiful    universe.";
+            objWordsOperation.DisplayDistinctWords(lsParagraph);
+
+            /* Test Case 4 */
+            /* With some Numbers */
+            /* Expected Output
+                The --> 1
+                paragraph --> 2
+                2 --> 1
+                is --> 1
+                very --> 1
+                lengthy --> 1
+                Need --> 1
+                to --> 1
+                split --> 1
+                this --> 1             
+             */
+            /*  Testing ==> SUCCESS */
+            lsParagraph = "The paragraph 2 is very lengthy. Need to split this paragraph.";
+            objWordsOperation.DisplayDistinctWords(lsParagraph);
+
+            /* Test Case 5 */
+            /* With some Lengthy Paragraph ==> SUCCESS. */
+            lsParagraph = "Bursting with imagery, motion, interaction and distraction though it is, today’s World Wide Web is still primarily a conduit for textual information. " +
+                          "In HTML5, the focus on writing and authorship is more pronounced than ever. It’s evident in the very way that new elements such as article and aside are named. " +
+                          "HTML5 asks us to treat the HTML document more as well, a document.";
+
+            objWordsOperation.DisplayDistinctWords(lsParagraph);
+
+
+            /* Test Casee 6 --> With decimal numbers. */
+            /* Iteration 1 --> FAILED. 8.1 is returned as seperate two words which is wrong. */
+            /* Iteration 2 --> SUCCESS. 8.1 is returned as one single words which is correct. */
+            /* After this fix, RE-Ran all the above test cases. They passed successfully. */
+            lsParagraph = "My phone is 8.1 version";
+            objWordsOperation.DisplayDistinctWords(lsParagraph);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DistinctWords/WordsOperations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistinctWords
+{
+    public class WordsOperations
+    {
+
+        public void DisplayDistinctWords(string lsinputParagraph)
+        {
+            List<string> wordsList;
+            List<string> distinctList;
+            char[] seperators = new char[] { ' ', ',' };
+            int count = 0;
+
+            try
+            {
+
+                if (String.IsNullOrEmpty(lsinputParagraph))
+                {
+                    Console.WriteLine("Input paragraph is empty.");
+                    return;
+                }
+
+                /* Split the Input paragraph by word seperators. */
+                wordsList = lsinputParagraph.Split(seperators, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                distinctList = wordsList.Select(x => x.TrimEnd('.')).Distinct(StringComparer.CurrentCultureIgnoreCase).ToList();
+
+                /* Get the list of distinct words and count of each word in the paragraph. */
+                foreach (string word in distinctList)
+                {
+                    count = wordsList.FindAll(x => x.TrimEnd('.').Equals(word, StringComparison.CurrentCultureIgnoreCase)).Count();
+
+                    Console.WriteLine(word + " --> " + count.ToString());
+                }                
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
